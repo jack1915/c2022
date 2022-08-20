d2f3054131fcc5e79e9bb02d920c496d15c2debb
Jack <pochta1915@mail.ru>
2022-08-20 20:48:31 +0500

    create 2022

--- Program.cs
+++ Program.cs
@@ -1,79 +1,24 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.Write("Введите ряд чисел, разделенных запятой : ");
-string? seriesOfNumbers = Console.ReadLine();
+﻿// Напишите программу, которая задаёт массив из 8 элементов 
+// и выводит их на экран.
 
-seriesOfNumbers = seriesOfNumbers + ",";    // дополнительня запятая для обозначения конца строки
-
-// функция удаления пробелов из строки 
-string RemovingSpaces (string series){
-  string seriesNew = "";
-  for (int i = 0; i < series.Length; i++)
+// 1, 2, 5, 7, 19, 6, 1, 33 -> [1, 2, 5, 7, 19, 6, 1, 33]
+int[] numbers = new int[8];
+Console.Write( "[");
+void Array(int[] array, int min , int max){
+  for (int i = 0; i<array.Length; i++ ){
+    array[i] = new Random().Next(-10000, 10000);
+  }  
+}
+void WriteArray(int[] array){
+    for (int i = 0; i<7; i++ )   
   {
-    if (series[i] != ' ') 
-    {
-      seriesNew += series[i];
-    }
+    Console.Write(array[i] + ", ");
   }
-  return seriesNew;
-}
-
-//  функция  проверки на правильность ввода 
-void СheckNumber2 (int  series){
-
-      if (series == '0'||series == '1'||series == '2'
-      ||series == '3'||series == '4'||series == '5'||series == '6'
-      ||series == '7'||series == '8'||series == '9'||series == ','
-      ||series == '-')
-      {
-      }
-        else {
-          Console.WriteLine($"Ошибка ввода  символа. Вводите цифры.");
-
-      }
-}
-
-// функция  создания и заполнения массива из строки
-int[] ArrayOfNumbers(string seriesNew){ 
-
-  int[] arrayOfNumbers = new int[1];    // инициализация массива из 1 элемента
-
-  int j =0;
-
-  for (int i = 0; i < seriesNew.Length; i++){
-    string seriesNew1 = "";
-
-    while (seriesNew[i] != ',' && i < seriesNew.Length){
-      seriesNew1 += seriesNew[i];
-      СheckNumber2(seriesNew[i]);
-      i++;
-    }
-    arrayOfNumbers[j] = Convert.ToInt32(seriesNew1);    // заполняет массив значениями из строки
-    if (i < seriesNew.Length-1){
-      arrayOfNumbers = arrayOfNumbers.Concat(new int[] {0}).ToArray();    // добавляет новый нулевой элемент в конец массива
-    }
-    j++;
-  }
-  return arrayOfNumbers;
-}
-
-// функция  вывода массива на печать 
-void PrintArry(int[] coll){
-  int count = coll.Length;
-  int index = 0;
-  Console.Write("[");
-  while(index < count){
-    Console.Write(coll[index]);
-    index++;
-    if (index < count){
-      Console.Write(", ");
-    }
-  }
+  Console.Write(array[7]);
   Console.Write("]");
-} 
+  Console.WriteLine();
+}
+Array(numbers, -1000, 1000);
+WriteArray(numbers);
 
 
-string seriesNew = RemovingSpaces(seriesOfNumbers);
-
-int[] arrayOfNumbers =  ArrayOfNumbers(seriesNew);
-
-PrintArry(arrayOfNumbers);
